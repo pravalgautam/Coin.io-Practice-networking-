717a6db86c50466c489c42d57cd383324df4ff26
pravalgautam <pravalgautam79@gmail.com>
2023-10-31 23:16:31 +0530

    Initial Commit

---
+++ Coin.io/Coin_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coin_ioApp.swift
+//  Coin.io
+//
+//  Created by Praval Gautam on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Coin_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
